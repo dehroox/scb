02ba222829b1fa537070824eabd3d12d0d89e6a5
dehroox <dehrexd@gmail.com>
2025-08-16 18:41:06 +0800

    docs

--- include/breadfile.h
+++ include/breadfile.h
@@ -6,6 +6,8 @@
 /*
     Reads file into `out`, returns 0 if failed, returns the filesize when it
     succeeds.
+
+    DO NOT free() THE BUFFER! USE munmap() instead.
 */
 size_t breadfile(const char* path, void** out);
 
