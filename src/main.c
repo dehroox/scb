5ce30227fe184dbf2aa9080c3b3d40d59f3c0214
dehroox <dehrexd@gmail.com>
2025-08-16 14:48:00 +0800

    yeye

--- src/main.c
+++ src/main.c
@@ -11,29 +11,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "likely_unlikely.h"
 
 #define VERSION "0.1"
 
 static void usage() {
-    (void)fprintf(
-        stderr,
-        "Usage: SCB [options] <args>\n"
-        "Options:\n"
-        "  -h                   Show this help message\n"
-        "  -v                   Show version\n"
-        "  -c <path>            Explicitly set config file\n"
-        "  -a <dep(s)>          Add a dependency\n"
-        "  -r <dep(s)>          Remove a dependency\n"
-        "  -i <path>            Initialize a project\n"
-        "  -r <?profile>        Run default or a specified profile\n"
-        "  -b <?profile>        Build default or a specified profile\n");
+    (void)fprintf(stderr,
+                  "Usage: SCB [options] <args>\n"
+                  "Options:\n"
+                  "  -h                   Show this help message\n"
+                  "  -v                   Show version\n"
+                  "  -c <path>            Explicitly set config file\n"
+                  "  -a <dep(s)>          Add a dependency\n"
+                  "  -r <dep(s)>          Remove a dependency\n"
+                  "  -i <path>            Initialize a project\n"
+                  "  -r <?profile>        Run default/specified profile\n"
+                  "  -b <?profile>        Build default/specified profile\n");
 }
 
 int main(int argc, char* argv[]) {
     char cwd[1024];
     char* config_file = NULL;
 
-    (void)getcwd(cwd, sizeof cwd);
+    if (unlikely(!getcwd(cwd, sizeof cwd))) {
+        (void)fprintf(stderr, "Failed to get CWD, exiting.");
+        return EXIT_FAILURE;
+    }
 
     char arg = (argc > 1) ? argv[1][1] : '?';
 
