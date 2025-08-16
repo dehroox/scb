35cea496219c5c519c7080ebca8f5ecab3ef21a3
dehroox <dehrexd@gmail.com>
2025-08-16 15:07:29 +0800

    ,

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
         main.c  ~  poem
 
         the cli frontend, this file shall contain;
-        a simple program, it shall be;
-        no tui, no interactivity, no fluff;
+        a simple program, it shall be,
+        no tui, no interactivity, no fluff,
         neither will it have butts, nor cuts, nor any coconuts.
 */
 
