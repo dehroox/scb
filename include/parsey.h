207443a3186a9559294b2b1386abf294d021234b
dehroox <dehrexd@gmail.com>
2025-08-16 19:26:25 +0800

    fix

---
+++ include/parsey.h
@@ -0,0 +1,7 @@
+#ifndef PARSEY_H
+#define PARSEY_H
+
+#include "state.h"
+State parsey(void** input);
+
+#endif
---
+++ src/parsey.c
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+#include "parsey.h"
+#include "state.h"
+#include <yaml.h>
+
+State parsey(void** input) {
+    
+}
+
