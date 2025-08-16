c76570c2a7914d9e4ae840e9ecfcaf6c02470c2e
dehroox <dehrexd@gmail.com>
2025-08-16 12:02:56 +0800

    scaffold

--- src/main.c
+++ src/main.c
@@ -8,7 +8,34 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-        printf("Hello World");
+static void usage() {
+    (void)fprintf(stderr,
+                  "Usage: SCB [options] <args>\n"
+                  "Options:\n"
+                  "  -h, --help       Show this help message\n"
+                  "  -v, --version    Show version\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            usage();
+            return EXIT_SUCCESS;
+        }
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
+            puts("SCB -- v0.1");
+            return EXIT_SUCCESS;
+        }
+
+        printf("Unrecognized option: %s\n", argv[i]);
+        return EXIT_FAILURE;
+    }
 }
