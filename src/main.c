e766e944efd3d2b28ff349b661f2f192c1c78709
dehroox <dehrexd@gmail.com>
2025-08-16 12:21:53 +0800

    better arg handling

--- src/main.c
+++ src/main.c
@@ -9,20 +9,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 static void usage() {
-    (void)fprintf(
-        stderr,
-        "Usage: SCB [options] <args>\n"
-        "Options:\n"
-        "  -h, --help                     Show this help message\n"
-        "  -v, --version                  Show version\n"
-        "  -c, --compiler <cc>            Explicitly set compiler\n"
-        "  -C, --config <path>            Explicitly set config file\n"
-        "  -a, --add <dep(s)>             Install a dependency\n"
-        "  -r, --remove <dep(s)>          Remove a dependency\n"
-        "  -i, --initialize <path>        Initialize a project\n");
+    (void)fprintf(stderr,
+                  "Usage: SCB [options] <args>\n"
+                  "Options:\n"
+                  "  -h                   Show this help message\n"
+                  "  -v                   Show version\n"
+                  "  -c <cc>              Explicitly set compiler\n"
+                  "  -C <path>            Explicitly set config file\n"
+                  "  -a <dep(s)>          Install a dependency\n"
+                  "  -r <dep(s)>          Remove a dependency\n"
+                  "  -i <path>            Initialize a project\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -31,17 +29,15 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+    char arg = argv[1][1];
+
+    switch (arg) {
+        case 'h':
             usage();
             return EXIT_SUCCESS;
-        }
-        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
-            puts("SCB -- v0.1");
-            return EXIT_SUCCESS;
-        }
 
-        printf("Unrecognized option: %s\n", argv[i]);
-        return EXIT_FAILURE;
+        default:
+            (void)fprintf(stderr, "Unrecognized option: %s\n", argv[1]);
+            return EXIT_FAILURE;
     }
 }
