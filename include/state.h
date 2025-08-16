a2e16e593918a4e06c4c2395757bff831279891b
dehroox <dehrexd@gmail.com>
2025-08-16 19:08:28 +0800

    package struct

--- include/state.h
+++ include/state.h
@@ -1,16 +1,24 @@
 #ifndef STATE_H
 #define STATE_H
 
+#include <stddef.h>
 typedef struct {
-    char* cc;
-    char* flags;
-    char* files;
-    char* out;
+    const char* cc;
+    const char* flags;
+    const char* files;
+    const char* out;
 } Profile;
+
+typedef struct {
+    const char* name;
+    const char* version;
+} Package;
 
 typedef struct {
     Profile global;
     Profile selected;
+    size_t package_num;
+    Package* packages;
 } State;
 
 #endif
